40a67eea3aaa71b18fa9084682873bba2e30f6fa
Daniel Thompson <dthomp92@gmail.com>
2025-06-12 20:49:51 -0500

    started working on job job

--- ai_plays_jackbox/bot/bot_factory.py
+++ ai_plays_jackbox/bot/bot_factory.py
@@ -14,6 +14,7 @@ BOT_TYPES: dict[str, type[JackBoxBotBase]] = {
     "rapbattle": MadVerseCityBot,
     "jokeboat": JokeBoatBot,
     "ridictionary": DictionariumBot,
+    # "apply-yourself": JobJobBot,
 }
 
 
